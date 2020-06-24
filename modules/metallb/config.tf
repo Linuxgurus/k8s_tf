3359170b8d3b1462bb9acbf72d27d785386bcca6
James Blackwell <jblack@linuxguru.net>
2020-06-24 13:14:21 -0700

    fix address pools

--- modules/metallb/config.tf
+++ modules/metallb/config.tf
@@ -11,18 +11,10 @@ resource "kubernetes_config_map" "metallb" {
 
 data "template_file" "config" {
   template = <<EOT
-  apiVersion: v1
-  kind: ConfigMap
-  metadata:
-  namespace: metallb
-  name: metallb-config
-  data:
-  config: |
-  address-pools:
-  - name: default
+address-pools:
+- name: default
   protocol: layer2
   addresses:
   - ${var.addresses}
-  EOT
+EOT
 }
-
