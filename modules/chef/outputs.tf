a954dbd0fafdadc18a7d93db3859dc7f5c0e3694
James Blackwell <jblack@linuxguru.net>
2020-07-03 13:17:22 -0700

    we need to know where chef is

--- modules/chef/outputs.tf
+++ modules/chef/outputs.tf
@@ -1,11 +1,4 @@
 
-data "kubernetes_service" "chef" {
-  metadata { 
-    name = local.chef_name
-    namespace = var.namespace
-  }
-}
-
 output "lb" {
-  value = data.kubernetes_service.chef.load_balancer_ingress.0.ip
+  value = kubernetes_service.chef.load_balancer_ingress.0.ip
 }
